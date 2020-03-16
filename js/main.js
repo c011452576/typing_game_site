75420d3b068a019aeb2c3b1c948f1bdd3aa5b7a0
c011452576 <c011452576@edu.teu.ac.jp>
2020-03-16 11:32:20 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,97 @@
+'use strict';
+
+{
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'set',
+  ];
+  let word = words[Math.floor(Math.random() * words.length)];
+  let loc = 0;
+  let score = 0;
+  let miss = 0;
+  const timeLimit = 10 * 1000;
+  let startTime;
+  let isPlaying = false;
+
+  const target = document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
+  const timerLabel = document.getElementById('timer');
+
+  // WORD
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++){
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+
+  // TIMER
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+
+    const timeoutId = setTimeout(() => {
+      updateTimer();
+    }, 10);
+
+    if (timeLeft < 0) {
+      isPlaying = false;
+      clearTimeout(timeoutId);
+      timerLabel.textContent = '0.00';
+      setTimeout(() => {
+        showResult();
+      }, 100);
+
+      target.textContent = 'click to replay';
+    }
+  }
+
+  function showResult() {
+    const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
+    alert(`${score} letters, ${miss} misses, ${accuracy.toFixed(2)}% accuracy`);
+  }
+
+  // GAME START!!!
+  window.addEventListener('click', () => {
+    if (isPlaying){
+      return;
+    }
+    isPlaying = true;
+
+    loc;
+    score;
+    miss;
+    scoreLabel.textContent = score;
+    missLabel.textContent = miss;
+    word = words[Math.floor(Math.random() * words.length)];
+
+    target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
+  });
+
+  // TYPING NOW
+  window.addEventListener('keydown', e =>{
+    if (isPlaying !== true) {
+      return;
+    }
+    if (word[loc] === e.key) {
+      loc++;
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
+      updateTarget();
+      score++;
+      scoreLabel.textContent = score;
+    } else {
+      miss++;
+      missLabel.textContent = miss;
+    }
+  });
+}
